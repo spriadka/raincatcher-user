8a93108814eb51cacba1a1d44bd549b026602f11
spriadka <spriadka@github.com>
2017-05-16 13:33:57 +0200

    coveralls trying

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,6 @@ module.exports = function(grunt) {
     eslint: {
       src: ["lib/**/*.js"]
     },
-
     mochaTest: {
       test: {
         src: ['test/**/*-spec.js'],
@@ -20,17 +19,41 @@ module.exports = function(grunt) {
         }
       }
     },
+    mocha_istanbul: {
+      coverage: {
+        src: ['test/**/*.js']
+      },
+      coveralls: {
+        src: 'test/**/*-spec.js',
+        options: {
+          coverage: true,
+          root: './lib'
+        }
+
+      }
+    },
     env:{
       test :{
         WFM_USE_MEMORY_STORE: true
       }
     }
   });
+  grunt.event.on('coverage',function(lcov,done){
+    require('coveralls').handleInput(lcov, function(err){
+      if (err) {
+        return done(err);
+      }
+      done();
+    });
+  });
+  grunt.loadNpmTasks('grunt-mocha-istanbul');
   grunt.loadNpmTasks('grunt-env');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks("grunt-eslint");
   grunt.registerTask('mocha', ['mochaTest']);
   grunt.registerTask('unit', ['env:test', 'eslint', 'mocha']);
+  grunt.registerTask('coveralls',['env:test','mocha_istanbul:coveralls']);
+  grunt.registerTask('coverage',['env:test','mocha_istanbul:coverage']);
   grunt.registerTask('default', ['unit']);
 
 };
